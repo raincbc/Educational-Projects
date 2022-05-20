93c72c5d805779dc727fbc7ecad688f7a80f71b7
raincbc <raincbc@gmail.com>
2022-05-20 14:31:29 +0300

    jq

--- Final_Project/script/script.js
+++ Final_Project/script/script.js
@@ -19,3 +19,20 @@ $('#menuToggle #toggleCheckbox').change(function() {
         $('body').css({'overflow': "visible" })
     }
 })
+
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 500){
+        $('#up').fadeIn();
+    } else {
+        $('#up').fadeOut();
+    }
+})
+
+$('#up').on('click', function () {
+    $('html, body').animate({scrollTop: 0},600)
+})
+
+$('#home').on('click', function (e) {
+    e.preventDefault();
+    $('html, body').animate({scrollTop: $('#about').offset().top},600)
+})
