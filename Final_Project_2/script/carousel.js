ad947690048305e5befbe0971693257d44cfc104
raincbc <raincbc@gmail.com>
2022-06-02 19:35:55 +0300

    jq

--- Final_Project_2/script/carousel.js
+++ Final_Project_2/script/carousel.js
@@ -3,23 +3,58 @@ $('.carousel').slick({
     slidesToShow: 3,
     slidesToScroll: 3,
     prevArrow: $('.slick-arrow__prev'),
-    nextArrow: $('.slick-arrow__next')
-});
+    nextArrow: $('.slick-arrow__next'),
+    responsive:[
+            {
+                breakpoint: 900,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            },
+        ]
+    })            
+
+    
+
 
 $('.gallery-content').slick({
     slidesToShow: 2,
     slidesToScroll: 1,
     prevArrow: $('.gallery-slick__arrow-prev'),
-    nextArrow: $('.gallery-slick__arrow-next')
+    nextArrow: $('.gallery-slick__arrow-next'),
+    responsive:[
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            }    
+        ]
 })
 
 $('.customers-content__slider').slick({
     slidesToShow: 2,
     slidesToScroll: 2,
     prevArrow: $('.customer-slick__arrow-prev'),
-    nextArrow: $('.customer-slick__arrow-next')
+    nextArrow: $('.customer-slick__arrow-next'),
+    responsive:[
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            }    
+        ]
 })
-
-
 
 
